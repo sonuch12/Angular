35d28a3e0f5e8aea6fb29b9be82fe9ed9bae203c
Sonu <sonu121986@gmail.com>
2023-04-04 16:52:53 +0530

    3rd commit Componentcreated

--- Angular_Project/AngularComponent/src/app/app.module.ts
+++ Angular_Project/AngularComponent/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
-  declarations: [AppComponent, ContainerComponent],
+  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent, NotificationComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Angular_Project/AngularComponent/src/app/notification/notification.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<p>Using Notification as a component</p>',
+  styles: [' p{ font-size: 18px; text-align: center;}', ''],
+})
+export class NotificationComponent {}
