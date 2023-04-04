f0420be1aa795dc3bc39485ecb51aeeb0db7cda8
Sonu <sonu121986@gmail.com>
2023-04-04 18:50:18 +0530

    Databindina

--- Angular_Project/AngularComponent/src/app/nav/nav.component.ts
+++ Angular_Project/AngularComponent/src/app/nav/nav.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  selector: '[app-nav]',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-
-}
+export class NavComponent {}
--- Angular_Project/AngularComponent/src/app/notification/notification.component.ts
+++ Angular_Project/AngularComponent/src/app/notification/notification.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: '<p>Using Notification as a component</p>',
+  template: `<div class="alert alert-warning">
+    <p>This is notification</p>
+  </div>`,
   styles: [' p{ font-size: 18px; text-align: center;}', ''],
 })
 export class NotificationComponent {}
