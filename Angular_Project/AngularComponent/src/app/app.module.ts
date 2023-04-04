672b9449af9c6f0204ca870fbed8f1d8151f5d63
Sonu <sonu121986@gmail.com>
2023-04-04 16:11:19 +0530

    Component Creation

---
+++ Angular_Project/AngularComponent/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+
+@NgModule({
+  declarations: [AppComponent, ContainerComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
